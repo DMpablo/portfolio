c1286a2fc11e02e97d96290d506d704257e253d9
DMpablo <phdemarco@gmail.com>
2022-03-18 15:29:24 -0300

    Updates

---
+++ precache-manifest.3eb63cf242135c794301f35261939f24.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "f69d92205c8d75a7c825",
+    "url": "/portfolio/static/css/main.70cf266f.chunk.css"
+  },
+  {
+    "revision": "f69d92205c8d75a7c825",
+    "url": "/portfolio/static/js/main.f69d9220.chunk.js"
+  },
+  {
+    "revision": "217be5ca9efc2ced57b7",
+    "url": "/portfolio/static/js/1.217be5ca.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "4776f2b3aecfd2c132893075b5703fd3",
+    "url": "/portfolio/static/media/bxl-html5.4776f2b3.svg"
+  },
+  {
+    "revision": "7c4d92c2e2b9236ee2caaaafac80b91f",
+    "url": "/portfolio/static/media/bxl-sass.7c4d92c2.svg"
+  },
+  {
+    "revision": "c39ec4f528a0250b108b87868b051e58",
+    "url": "/portfolio/static/media/bxl-git.c39ec4f5.svg"
+  },
+  {
+    "revision": "d034e91c48877eab6dc2fe130a906510",
+    "url": "/portfolio/static/media/bxl-github.d034e91c.svg"
+  },
+  {
+    "revision": "4b9ae4f75fbc54d39dfd13da9b109844",
+    "url": "/portfolio/static/media/bxl-javascript.4b9ae4f7.svg"
+  },
+  {
+    "revision": "99e67caa3af8b5274ab450474babad84",
+    "url": "/portfolio/static/media/bxl-react.99e67caa.svg"
+  },
+  {
+    "revision": "dc3779667265e28abe4d1015c631f920",
+    "url": "/portfolio/static/media/bxl-css3.dc377966.svg"
+  },
+  {
+    "revision": "27cb103fb8a81c2cd9c388bc827f7958",
+    "url": "/portfolio/static/media/bxs-file-json.27cb103f.svg"
+  },
+  {
+    "revision": "8792c3f1bb017514695de8e0ac66db41",
+    "url": "/portfolio/static/media/workana.8792c3f1.svg"
+  },
+  {
+    "revision": "9dbc79dab48422c821c75bbfc21165ec",
+    "url": "/portfolio/static/media/gmail.9dbc79da.svg"
+  },
+  {
+    "revision": "831a9224507c33dbeaa85a3b78a76b7d",
+    "url": "/portfolio/static/media/linkedin.831a9224.svg"
+  },
+  {
+    "revision": "cdc7ab710ef59536df9f47d0710b9526",
+    "url": "/portfolio/index.html"
+  }
+];
